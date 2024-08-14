0a4b99ce5c1c5436e653c61ba61ba3cea94d30bf
Malte Ihlefeld <malte@famihlefeld.de>
2024-08-14 15:37:22 +0200

    added sitemap

--- src/settings.js
+++ src/settings.js
@@ -20,7 +20,7 @@ var defaultSettings = {
     'scrambleSize': 2,
     'weightedChoice': true,
     'colors': {},
-    'selectedAlgSets': {}
+    'selectedAlgSets': {},
 };
 
 var currentSettings = {};
