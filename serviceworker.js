1ed24eef79b60e18fbcef46d010d8bb246339484
Malte Ihlefeld <malte@famihlefeld.de>
2026-01-30 17:36:06 +0100

    bump version

--- serviceworker.js
+++ serviceworker.js
@@ -21,7 +21,7 @@ var assets = [
   "https://colorjs.io/dist/color.global.js",
   "https://cdn.jsdelivr.net/npm/chart.js"
 ];
-const trainerCache = "alg-trainer-cache-1.5.4";
+const trainerCache = "alg-trainer-cache-1.5.5";
 
 function refreshCache() {
   caches.keys().then((keys) => {
